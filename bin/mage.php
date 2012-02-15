e8e436f52ea340cddde770d01c73aa84a2356ce7
Andrs Montaez <andresmontanez@gmail.com>
2012-02-15 18:46:26 -0200

    Version 0.9.3

--- bin/mage.php
+++ bin/mage.php
@@ -24,7 +24,7 @@ date_default_timezone_set('UTC');
 
 $baseDir = dirname(dirname(__FILE__));
 
-define('MAGALLANES_VERSION', '0.9.2');
+define('MAGALLANES_VERSION', '0.9.3');
 
 require_once $baseDir . '/Mage/spyc.php';
 require_once $baseDir . '/Mage/Autoload.php';
