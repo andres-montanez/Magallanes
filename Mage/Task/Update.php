e42a1f11938cade15d2fd963e4e6f813f76446b0
Andrs Montaez <andresmontanez@gmail.com>
2012-02-15 18:52:35 -0200

    Update Fix.

--- Mage/Task/Update.php
+++ Mage/Task/Update.php
@@ -7,17 +7,16 @@ class Mage_Task_Update
     {
         $this->_config = $config;
 
-        $taskConfig = $config->getConfig();
-        $task = Mage_Task_Factory::get('scm/update', $taskConfig);
+        $task = Mage_Task_Factory::get('scm/update', $config);
         $task->init();
         
-        Mage_Console::output( PHP_TAB . 'Updating application via ' . $task->getName() . ' ... ');
+        Mage_Console::output('Updating application via ' . $task->getName() . ' ... ', 1, 0);
         $result = $task->run();
         
         if ($result == true) {
-            Mage_Console::output(PHP_TAB . 'OK' . PHP_EOL);
+            Mage_Console::output('OK' . PHP_EOL, 0);
         } else {
-            Mage_Console::output(PHP_TAB . 'FAIL' . PHP_EOL);
+            Mage_Console::output( 'FAIL' . PHP_EOL, 0);
         }
     }
 
