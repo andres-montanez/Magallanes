c9759b19c0be24c2ca3c61866231973d7dca0f63
Andrés Montañez <andresmontanez@gmail.com>
2013-10-13 13:30:55 -0200

    Add List Environment command.
    mage list environments

---
+++ Mage/Command/BuiltIn/List.php
@@ -0,0 +1,42 @@
+<?php
+class Mage_Command_BuiltIn_List
+    extends Mage_Command_CommandAbstract
+{
+    public function run()
+    {
+        $subCommand = $this->getConfig()->getArgument(1);
+
+        try {
+            switch ($subCommand) {
+                case 'environments':
+                    $this->_environment();
+                    break;
+            }
+        } catch (Exception $e) {
+            Mage_Console::output('<red>' . $e->getMessage() . '</red>', 1, 2);
+        }
+    }
+
+    private function _environment()
+    {
+    	$environments = array();
+        $content = scandir('.mage/config/environment/');
+        foreach ($content as $file) {
+            if (strpos($file, '.yml') !== false) {
+            	$environments[] = str_replace('.yml', '', $file);
+            }
+        }
+        sort($environments);
+
+        if (count($environments) > 0) {
+        	Mage_Console::output('<dark_gray>These are your configured environments:</dark_gray>', 1, 1);
+        	foreach ($environments as $environment) {
+        		Mage_Console::output('* <light_red>' . $environment . '</light_red>', 2, 1);
+        	}
+        	Mage_Console::output('', 1, 1);
+
+        } else {
+        	Mage_Console::output('<dark_gray>You don\'t have any environment configured.</dark_gray>', 1, 2);
+        }
+    }
+}
