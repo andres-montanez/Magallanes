1653b6354db602e4deddfe1ecc17d38c752708fe
Andrés Montañez <andresmontanez@gmail.com>
2013-11-07 23:12:30 -0200

    Add Magento Tasks for clearing cache. Features by Oscar Reales.

---
+++ Mage/Task/BuiltIn/Magento/ClearCacheTask.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * This file is part of the Magallanes package.
+*
+* (c) Andrés Montañez <andres@andresmontanez.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Mage\Task\BuiltIn\Magento;
+
+use Mage\Task\AbstractTask;
+
+use Exception;
+
+/**
+ * Task for Clearing Cache
+ *
+ * @author Oscar Reales <oreales@gmail.com>
+ */
+class ClearCacheTask extends AbstractTask
+{
+	/**
+	 * (non-PHPdoc)
+	 * @see \Mage\Task\AbstractTask::getName()
+	 */
+    public function getName()
+    {
+        return 'Magento - Clean Cache [built-in]';
+    }
+
+    /**
+     * Clears Cache
+     * @see \Mage\Task\AbstractTask::run()
+     */
+    public function run()
+    {
+        $command = 'rm -rf var/cache/*';
+        $result = $this->runCommandLocal($command);
+
+        return $result;
+    }
+}
