6f551cabf00e73bd1b6efc91e2ff1357a807715c
Alex V Kotelnikov <gudron@gudron.me>
2014-09-28 00:42:15 +0400

    added relative linking @ Link shared files task

--- Mage/Task/BuiltIn/Filesystem/LinkSharedFilesTask.php
+++ Mage/Task/BuiltIn/Filesystem/LinkSharedFilesTask.php
@@ -8,6 +8,8 @@ use Mage\Task\SkipException;
 class LinkSharedFilesTask extends AbstractTask implements IsReleaseAware
 {
 
+    const ABSOLUTE_LINKING = 'absolute';
+    const RELATIVE_LINKING = 'relative';
     /**
      * Returns the Title of the Task
      * @return string
@@ -25,25 +27,33 @@ class LinkSharedFilesTask extends AbstractTask implements IsReleaseAware
      */
     public function run()
     {
-        $linkedFiles = $this->getParameter('linked_files', []);
-        $linkedFolders = $this->getParameter('linked_folders', []);
+        $linkedFiles    = $this->getParameter('linked_files', []);
+        $linkedFolders  = $this->getParameter('linked_folders', []);
+        $linkingStrategy = $this->getParameter('linking_stategy', self::ABSOLUTE_LINKING);
+
+        $linkedEntities = array_merge($linkedFiles,$linkedFolders);
+
         if (sizeof($linkedFiles) == 0 && sizeof($linkedFolders) == 0) {
             throw new SkipException('No files and folders configured for sym-linking.');
         }
 
         $sharedFolderName = $this->getParameter('shared', 'shared');
-        $sharedFolderName = rtrim($this->getConfig()->deployment('to'), '/') . '/' . $sharedFolderName;
+        $sharedFolderPath = rtrim($this->getConfig()->deployment('to'), '/') . '/' . $sharedFolderName;
         $releasesDirectory = $this->getConfig()->release('directory', 'releases');
-        $releasesDirectory = rtrim($this->getConfig()->deployment('to'), '/') . '/' . $releasesDirectory;
+        $releasesDirectoryPath = rtrim($this->getConfig()->deployment('to'), '/') . '/' . $releasesDirectory;
 
-        $currentCopy = $releasesDirectory . '/' . $this->getConfig()->getReleaseId();
-        foreach ($linkedFolders as $folder) {
-            $command = "ln -nfs $sharedFolderName/$folder $currentCopy/$folder";
-            $this->runCommandRemote($command);
-        }
+        $currentCopy = $releasesDirectoryPath . '/' . $this->getConfig()->getReleaseId();
+        if($linkingStrategy==self::RELATIVE_LINKING)
+            $relativeDiffPath = str_replace($this->getConfig()->deployment('to'),'',$currentCopy) . '/';
 
-        foreach ($linkedFiles as $folder) {
-            $command = "ln -nfs $sharedFolderName/$folder $currentCopy/$folder";
+        foreach ($linkedEntities as $entityPath) {
+            $sharedEntityLinkedPath = "$sharedFolderPath/$entityPath";
+            if($linkingStrategy==self::RELATIVE_LINKING) {
+                $parentFolderPath = dirname($entityPath);
+                $relativePath = empty($parentFolderPath)?$relativeDiffPath:$relativeDiffPath.$parentFolderPath.'/';
+                $sharedEntityLinkedPath = ltrim(preg_replace('/(\w+\/)/', '../', $relativePath),'/').$sharedFolderName .'/'. $entityPath;
+            }
+            $command = "ln -nfs $sharedEntityLinkedPath $currentCopy/$entityPath";
             $this->runCommandRemote($command);
         }
 
