b2bcbfb1e550c80091bdd281700909f89df09e20
Alex V Kotelnikov <gudron@gudron.me>
2014-10-27 22:08:32 +0400

    Rollback exception added

---
+++ Mage/Task/RollbackException.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the Magallanes package.
+*
+* (c) Alex V Kotelnikov <gudron@gudron.me>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Mage\Task;
+
+use Exception;
+
+/**
+ * Exception that indicates that the Task was Skipped
+ *
+ * @author Andrés Montañez <andres@andresmontanez.com>
+ */
+class RollbackException extends Exception
+{
+
+}
