d5ddd0b10476dc16a1ab25ba075cc7be0a16720e
Niels Maseberg <nielsmaseberg@hotmail.com>
2015-02-09 09:28:23 +0100

    Test for loading Tasks without Namespace

--- tests/MageTest/Task/FactoryTest.php
+++ tests/MageTest/Task/FactoryTest.php
@@ -13,6 +13,9 @@ class FactoryTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
+        $this->configMock = $this
+            ->getMock('Mage\Config');
+
         require_once __DIR__ . '/MyTaskExample.php';
     }
 
@@ -23,14 +26,28 @@ class FactoryTest extends PHPUnit_Framework_TestCase
     {
         $factory = new Factory;
 
-        $configMock = $this
-            ->getMockBuilder('Mage\Config')
-            ->getMock();
-
-        $myTask = $factory->get('My\Task\Example', $configMock);
+        $myTask = $factory->get('My\Task\Example', $this->configMock);
 
         $this->assertInstanceOf('My\Task\Example', $myTask);
 
+    }
+
+    /**
+     * @covers Mage\Task\Factory
+     */
+    public function testLoadClassWithoutNamespace()
+    {
+        $factory = new Factory();
+
+        $mock = $this
+            ->getMockBuilder('Mage\Task\AbstractTask')
+            ->setMockClassName('MyMageCustomTask')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $task = $factory->get('MyMageCustomTask', $this->configMock);
+
+        $this->assertInstanceof('MyMageCustomTask', $task);
     }
 }
 
