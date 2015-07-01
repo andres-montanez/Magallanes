28d9e9706aae8cde45b4648cad0f7ecd583a261b
AlberT <drAlberT@users.noreply.github.com>
2015-07-01 18:22:13 +0200

    Update Mailer.php

--- Mage/Mailer.php
+++ Mage/Mailer.php
@@ -73,18 +73,17 @@ class Mailer
             . 'Content-Transfer-Encoding: quoted-printable' . self::EOL
             . self::EOL
             . strip_tags(Console::getOutput()) . self::EOL
-			. self::EOL;
-
-	if ($this->logFile) {
-		$attachment = chunk_split(base64_encode(file_get_contents($this->logFile)));
-        $message .= '--Mage-mixed-' . $boundary . self::EOL
-		. 'Content-Type: text/plain; name="log.txt"' . self::EOL
-		. 'Content-Transfer-Encoding: base64' . self::EOL
-		. 'Content-Disposition: attachment' . self::EOL
-		. self::EOL
-		. $attachment . self::EOL
-		. '--Mage-mixed-' . $boundary . '--' . self::EOL;
-	}
+            . self::EOL;
+        if ($this->logFile) {
+            $attachment = chunk_split(base64_encode(file_get_contents($this->logFile)));
+            $message .= '--Mage-mixed-' . $boundary . self::EOL
+	        . 'Content-Type: text/plain; name="log.txt"' . self::EOL
+		    . 'Content-Transfer-Encoding: base64' . self::EOL
+		    . 'Content-Disposition: attachment' . self::EOL
+		    . self::EOL
+		    . $attachment . self::EOL
+		    . '--Mage-mixed-' . $boundary . '--' . self::EOL;
+		}
 
         mail($this->address, $subject, $message, $headers);
     }
