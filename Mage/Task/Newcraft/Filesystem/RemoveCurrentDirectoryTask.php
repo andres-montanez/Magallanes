eaa9c936a47d347784c29b1d530a489a20961d33
Bart Swaalf <siraic@gmail.com>
2015-10-15 17:55:47 +0200

    simplified remove current directory task

--- Mage/Task/Newcraft/Filesystem/RemoveCurrentDirectoryTask.php
+++ Mage/Task/Newcraft/Filesystem/RemoveCurrentDirectoryTask.php
@@ -12,12 +12,13 @@
 namespace Mage\Task\Newcraft\Filesystem;
 
 use Mage\Task\AbstractTask;
+use Mage\Task\Releases\IsReleaseAware;
 
 /**
  * Class RemoveCurrentDirectoryTask
  * @package Mage\Task\BuiltIn\Filesystem
  */
-class RemoveCurrentDirectoryTask extends AbstractTask
+class RemoveCurrentDirectoryTask extends AbstractTask implements IsReleaseAware
 {
 
     /**
@@ -25,28 +26,16 @@ class RemoveCurrentDirectoryTask extends AbstractTask
      */
     public function getName()
     {
-        return 'Removes existing current folder [newcraft]';
+        return 'Remove existing current folder if needed [newcraft]';
     }
 
     /**
-     * Removes any directory named current and replaces it with a symlink
+     * Removes any directory named current so it can be replaced with a symlink
      * @see \Mage\Task\AbstractTask::run()
      */
     public function run()
     {
-        $isDir = $this->runCommandRemote('cd ../../ && test -d current && test -h current && echo "y" || echo "n"', $output);
-        if( $isDir && $output == 'n') {
-            if($this->runCommandRemote('cd ../../ && rm -rf current', $output)) {
-                if($this->runCommandRemote('cd ../../ && test -d current && echo "y" || echo "n"', $output) && $output = 'n') {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-        } else {
-            return true;
-        }
+        $this->runCommandRemote('test ! -h current && rm -rf current');
+        return true;
     }
 }
