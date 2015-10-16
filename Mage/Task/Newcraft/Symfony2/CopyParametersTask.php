763a2d2ac63dfcfd703b79b2cb02f22366260972
Bart Swaalf <siraic@gmail.com>
2015-10-16 12:05:46 +0200

    simplify copy parameters task

--- Mage/Task/Newcraft/Symfony2/CopyParametersTask.php
+++ Mage/Task/Newcraft/Symfony2/CopyParametersTask.php
@@ -1,19 +1,11 @@
 <?php
-/*
- * This file is part of the Magallanes package.
-*
-* (c) Andrés Montañez <andres@andresmontanez.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
 
 namespace Mage\Task\Newcraft\Symfony2;
 
 use Mage\Task\BuiltIn\Symfony2\SymfonyAbstractTask;
 
 /**
- * Task for Dumping Assetics
+ * Task for Copying configuring use of environment specific parameters.yml file
  *
  * @author Andrés Montañez <andres@andresmontanez.com>
  */
@@ -29,35 +21,19 @@ class CopyParametersTask extends SymfonyAbstractTask
     }
 
     /**
-     * Dumps Assetics
+     * Set correct parameters.yml file
      * @see \Mage\Task\AbstractTask::run()
      */
     public function run()
     {
         $envName = $this->getConfig()->deployment('environment');
 
-        //First check if parameters.yml exists
-        if( $this->runCommandRemote('cd ./app/config && test -f parameters.yml && echo "y" || echo "n"', $output) && $output == 'y') {
-            if($this->runCommandRemote('cd ./app/config && rm parameters.yml')) {
-                return $this->copyParameters($envName);
-            } else {
-                return false;
-            }
-        } else {
-            return $this->copyParameters($envName);
-        }
+        //remove existing parameters file if needed.
+        $this->runCommandRemote('cd app/config && test -f parameters.yml && rm -f parameters.yml');
+
+        //copy environment specific parameters file
+        return $this->runCommandRemote('cd app/config && test -f parameters-'.$envName.'.yml && cp -fp parameters-'.$envName.'.yml parameters.yml');
+
     }
 
-    /**
-     * Copies the parameters-{envName}.yml to parameters.yml
-     * @param $envName
-     * @return bool
-     */
-    public function copyParameters($envName) {
-        if( $this->runCommandRemote('cd ./app/config && test -f parameters-'.$envName.'.yml && echo "y" || echo "n"', $output) && $output == 'y') {
-            return $this->runCommandRemote('cd ./app/config && cp -fp parameters-'.$envName.'.yml parameters.yml');
-        } else {
-            return false;
-        }
-    }
 }
