48484793ac95c291a9732fda3c5d3c031fb1e24e
Bart Swaalf <siraic@gmail.com>
2016-02-15 12:17:18 +0100

    added simple confirm-environment task to help prevent accidental deploys to productions

---
+++ Mage/Task/Newcraft/Communication/ConfirmEnvironmentTask.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Mage\Task\Newcraft\Communication;
+
+use Mage\Task\AbstractTask;
+use Mage\Console;
+
+class ConfirmEnvironmentTask extends AbstractTask
+{
+    /**
+     * Returns the Title of the Task
+     * @return string
+     */
+    public function getName()
+    {
+        return 'Confirm environment [newcraft]';
+    }
+
+    /**
+     * Runs the task
+     *
+     * @return boolean
+     */
+    public function run()
+    {
+        Console::output('');
+        Console::output('Deploying to <white>'.$this->getConfig()->getEnvironment().'</white>! Sure? (Y/N)... ', 3, 0);
+        $answer = strtoupper(Console::readInput());
+        Console::output('Cont... <purple>'.$this->getName().'</purple> ... ', 2, 0);
+        return 'Y' === $answer;
+    }
+}
