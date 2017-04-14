261015981b8f98701c5e9cc0758a046b508d0f85
Andrés Montañez <andresmontanez@gmail.com>
2017-04-14 15:35:27 -0300

    Add tests

---
+++ tests/Task/BuiltIn/Composer/BasicComposerTask.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * This file is part of the Magallanes package.
+ *
+ * (c) Andrés Montañez <andres@andresmontanez.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mage\Tests\Task\BuiltIn\Composer;
+
+use Mage\Task\BuiltIn\Composer\AbstractComposerTask;
+use Symfony\Component\Process\Process;
+
+/**
+ * Basic Composer Task
+ *
+ * @author Andrés Montañez <andresmontanez@gmail.com>
+ */
+class BasicComposerTask extends AbstractComposerTask
+{
+    public function getName()
+    {
+        return 'composer/help';
+    }
+
+    public function getDescription()
+    {
+        return '[Composer] Help';
+    }
+
+    public function execute()
+    {
+        $options = $this->getOptions();
+        $cmd = sprintf('%s help', $options['path']);
+
+        /** @var Process $process */
+        $process = $this->runtime->runCommand(trim($cmd));
+
+        return $process->isSuccessful();
+    }
+}
