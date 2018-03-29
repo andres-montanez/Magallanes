7860fea42591e7b1f5bb0c2b37db77d15bcb53c1
Andrés Montañez <andresmontanez@gmail.com>
2018-03-29 18:09:11 -0300

    Improve tests for php 7.2

--- src/Command/BuiltIn/DeployCommand.php
+++ src/Command/BuiltIn/DeployCommand.php
@@ -143,6 +143,9 @@ class DeployCommand extends AbstractCommand
     protected function runOnHosts(OutputInterface $output, $tasks)
     {
         $hosts = $this->runtime->getEnvOption('hosts');
+        if (!is_array($hosts) && !$hosts instanceof \Countable) {
+            $hosts = [];
+        }
         if (count($hosts) == 0) {
             $output->writeln(sprintf('    No hosts defined, skipping %s tasks', $this->getStageName()));
             $output->writeln('');
